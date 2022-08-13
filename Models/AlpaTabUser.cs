d742bf74a46b85d931e46a884dc47d8057af3c82
Alexandros <alexandros@raincode.com>
2022-08-13 14:34:40 +0200

    minor change

--- Models/AlpaTabUser.cs
+++ Models/AlpaTabUser.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using AplaTabApi.Helpers;
+using AlpaTabApi.Helpers;
 
 namespace AlpaTabApi.Models
 {
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using AlpaTabApi.Data;
+using AlpaTabApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,5 +24,79 @@ app.UseHttpsRedirection();
 app.MapGet("/", () => { return "Welcome to the api!"; });
 
 app.MapGet("/users", async (AlpaTabContext context) => await context.AlpaTabUsers.ToListAsync());
+
+app.MapGet("/users/{id}", async (AlpaTabContext context, int id) =>
+    await context.AlpaTabUsers.FindAsync(id) is AlpaTabUser user ? Results.Ok(user) : Results.NotFound($"User not found: {id}"));
+
+app.MapPost("/users", async (AlpaTabContext context, AlpaTabUser user) =>
+{
+    context.AlpaTabUsers.Add(user);
+    await context.SaveChangesAsync();
+    return Results.Ok(user);
+});
+
+app.MapPut("/users/{id}", async (AlpaTabContext context, AlpaTabUser user, int id) =>
+{
+    AlpaTabUser existingUsr = await context.AlpaTabUsers.FindAsync(id);
+    if (existingUsr == null) return Results.NotFound($"User not found: {id}");
+
+    existingUsr.FirstName = user.FirstName;
+    existingUsr.LastName = user.LastName;
+    existingUsr.UserType = user.UserType;
+    existingUsr.Password = user.Password;
+    existingUsr.NickName = user.NickName;
+    existingUsr.Email = user.Email;
+    await context.SaveChangesAsync();
+
+    return Results.Ok(user);
+});
+
+app.MapDelete("/users/{id}", async (AlpaTabContext context, int id) =>
+{
+    AlpaTabUser userToRemove = await context.AlpaTabUsers.FindAsync(id);
+    if (userToRemove == null) return Results.NotFound($"User not found: {id}");
+
+    context.AlpaTabUsers.Remove(userToRemove);
+    await context.SaveChangesAsync();
+    return Results.Ok(userToRemove);
+});
+
+
+app.MapGet("/transactions", async (AlpaTabContext context) => await context.TransactionsList.ToListAsync());
+
+app.MapGet("/transactions/{id}", async (AlpaTabContext context, int id) =>
+    await context.TransactionsList.FindAsync(id) is AlpaTabTransaction t ? Results.Ok(t) : Results.NotFound($"Transaction not found: {id}"));
+
+app.MapPost("/transactions", async (AlpaTabContext context, AlpaTabTransaction t) =>
+{
+    context.TransactionsList.Add(t);
+    await context.SaveChangesAsync();
+    return Results.Ok(t);
+});
+
+app.MapPut("/transactions/{id}", async (AlpaTabContext context, AlpaTabTransaction t, int id) =>
+{
+    AlpaTabTransaction existingT = await context.TransactionsList.FindAsync(id);
+    if (existingT == null) return Results.NotFound($"Transaction not found: {id}");
+
+    existingT.NickName = t.NickName;
+    existingT.Amount = t.Amount;
+    existingT.TransactionType = t.TransactionType;
+    existingT.Timestamp = t.Timestamp;
+    existingT.Description = t.Description;
+    await context.SaveChangesAsync();
+
+    return Results.Ok(existingT);
+});
+
+app.MapDelete("/transactions/{id}", async (AlpaTabContext context, int id) =>
+{
+    AlpaTabTransaction tToRemove = await context.TransactionsList.FindAsync(id);
+    if (tToRemove == null) return Results.NotFound($"Transaction not found: {id}");
+
+    context.TransactionsList.Remove(tToRemove);
+    await context.SaveChangesAsync();
+    return Results.Ok(tToRemove);
+});
 
 app.Run();
