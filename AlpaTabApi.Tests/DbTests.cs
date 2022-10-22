19159ee2c61ae9dc3a9476b1ef3af8d02161cd03
Alex <xenakis_alexandros@outlook.com>
2022-10-22 12:33:28 +0200

    v1 wip

---
+++ AlpaTabApi.Tests/DbTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace AlpaTabApi.Tests
+{
+    public class DbTestsTests
+    {
+        [Fact]
+        public async Task TestUniqueClientNicknameConstraint()
+        {
+            Assert.True(false);
+        }
+    }
+}
